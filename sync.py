c4460e48dbf844e9ce97e99aa08eac58c22248d1
Max Beutelspacher <max.beutelspacher@mailbox.org>
2019-01-19 12:07:45 +0100

    add begin/end

--- sync.py
+++ sync.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import click
 import requests
-from analysis import count_tags, filter_tags_by_date_range
+from analysis import count_tags, filter_tags_by_date_range, parse_begin_end
 from merge import extract_tags_from_file
 import datetime as dt
 import configparser
@@ -17,17 +17,22 @@ class BeeminderGoal:
         self.days = (d['daystamp'] for d in self.datapoints)
         self.gap = gap
 
-    def get_datapoints(self,):
+    def get_datapoints(self, ):
         url = 'https://www.beeminder.com/api/v1/users/{}/goals/{}/datapoints.json?auth_token={}'.format(
             self.user, self.goal, self.auth_token)
         response = requests.get(url)
         return sorted(response.json(), key=lambda x: x['timestamp'])
 
     def post_datapoint(self, daystamp, value):
+        if value == 0:
+            return 0
         url = 'https://www.beeminder.com/api/v1/users/{}/goals/{}/datapoints.json'.format(
             self.user, self.goal)
-        params = {'daystamp': daystamp, 'value': value,
-                  'auth_token': self.auth_token}
+        params = {
+            'daystamp': daystamp,
+            'value': value,
+            'auth_token': self.auth_token
+        }
         requests.post(url, data=params)
 
     def delete_data_for_day(self, daystamp):
@@ -49,15 +54,15 @@ class BeeminderGoal:
 
 def get_daily_count(tags):
     begin_dt = dt.datetime.fromtimestamp(min(tags))
-    begin_day = dt.date(year=begin_dt.year,
-                        month=begin_dt.month, day=begin_dt.day)
+    begin_day = dt.date(
+        year=begin_dt.year, month=begin_dt.month, day=begin_dt.day)
     end_day = dt.date.today()
     duration = end_day - begin_day
     days = [begin_day + dt.timedelta(d) for d in range(1, duration.days)]
     counts = {}
     for day in days:
         day_begin = dt.datetime.combine(day, dt.time())
-        day_end = dt.datetime.combine(day+dt.timedelta(1), dt.time())
+        day_end = dt.datetime.combine(day + dt.timedelta(1), dt.time())
         f_tags = filter_tags_by_date_range(tags, day_begin, day_end)
         counts[day_begin.strftime('%Y%m%d')] = count_tags(f_tags)
     return counts
@@ -66,7 +71,7 @@ def get_daily_count(tags):
 def sumTagTime(count, synctags, gap):
     value = 0
     for tag in synctags:
-        value += count.get(tag, 0)*gap
+        value += count.get(tag, 0) * gap
     return value
 
 
@@ -77,16 +82,33 @@ def parse_config(configfile):
 
 
 @click.command()
-@click.argument('path', type=click.Path(exists=True, readable=True, resolve_path=True))
+@click.argument(
+    'path', type=click.Path(exists=True, readable=True, resolve_path=True))
 @click.argument('goal')
-@click.option('-c', '--config', 'configfile', help='path to config', type=click.Path(exists=True, readable=True, resolve_path=True), default=os.path.expanduser('~/.bmndrrc'))
-@click.option('--tags', 'synctags', default='', help='comma seperated list of tags')
-@click.option('--gap', 'gap', default=0.75, help='average interval between tags')
-@click.option('--update', 'update', is_flag=True, help='if set update existing datapoints')
-def sync(path, goal, synctags, gap, update,configfile):
+@click.option(
+    '-c',
+    '--config',
+    'configfile',
+    help='path to config',
+    type=click.Path(exists=True, readable=True, resolve_path=True),
+    default=os.path.expanduser('~/.bmndrrc'))
+@click.option(
+    '--tags', 'synctags', default='', help='comma seperated list of tags')
+@click.option(
+    '--gap', 'gap', default=0.75, help='average interval between tags')
+@click.option(
+    '--update',
+    'update',
+    is_flag=True,
+    help='if set update existing datapoints')
+@click.option('-b', '--begin', 'begin', help='begin date', default=None)
+@click.option('-e', '--end', 'end', help='end date', default=None)
+def sync(path, goal, synctags, gap, update, configfile, begin, end):
     '''sync tags in to beeminder goal'''
+    begin, end = parse_begin_end(begin, end)
     synctags = set(synctags.split(','))
     tags = extract_tags_from_file(path)
+    tags = filter_tags_by_date_range(tags, begin, end)
     counts = get_daily_count(tags)
     config = parse_config(configfile)
     user = config['account']['user']
@@ -96,6 +118,7 @@ def sync(path, goal, synctags, gap, update,configfile):
         value = sumTagTime(count, synctags, gap)
         if day in bgoal.days:
             if (not update or bgoal.isConsistent(day, value)):
+                print(day, value)
                 continue
             else:
                 bgoal.delete_data_for_day(day)
